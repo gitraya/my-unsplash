302da80fab93be9da9f6f41ceea8e6f8242c8f19
Raya <mail.raya15@gmail.com>
2022-06-25 14:44:48 +0700

    add errorHandler and unknown endpoint

---
+++ server/middlewares/index.js
@@ -0,0 +1,15 @@
+module.exports.unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+module.exports.errorHandler = (err, req, res, next) => {
+  console.error(err.message);
+
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (err.name === "ValidationError") {
+    return res.status(400).json({ error: err.message });
+  }
+
+  next(err);
+};
