87c512700c13e9a78f727cf0e243cea12ac4012c
Raya <mail.raya15@gmail.com>
2022-06-26 12:51:35 +0700

    fix images data

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext, Fragment } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useInfiniteQuery } from "react-query";
 import axios from "axios";
 import Spinner from "./Spinner";
@@ -52,41 +52,39 @@ const Main = () => {
     }
   };
 
+  const images = data?.pages?.map(({ results }) => results).flat() || [];
+
   return (
     <main
       className={`mt-2 sm:mt-8 mb-2 px-6 sm:px-10 md:px-20 w-full flex flex-col ${
-        data?.pages?.length ? "" : "h-full"
+        images.length ? "" : "h-full"
       } ${isLoading ? "h-3/4" : ""}`}
     >
-      {data?.pages?.length > 0 ? (
+      {images.length > 0 ? (
         <div className="w-full masonry sm:masonry-sm md:masonry-md space-y-6">
-          {data.pages.map((page, i) => (
-            <Fragment key={i}>
-              {page.results.map((item) => (
-                <div
-                  className="break-inside shadow-sm rounded-lg relative"
-                  key={item.id}
+          {images.map((item) => (
+            <div
+              className="break-inside shadow-sm rounded-lg relative"
+              key={item.id}
+            >
+              <img
+                alt={item.name}
+                src={item.url}
+                className="w-full rounded-lg"
+              />
+              <div className="absolute top-0 left-0 w-full h-full flex flex-col p-5 bg-black/0 rounded-lg opacity-0 hover:opacity-100 hover:bg-black/50">
+                <button
+                  type="button"
+                  onClick={() => onDeleteImage(item.id)}
+                  className="w-min ml-auto text-rose-500 border-rose-500 border font-medium rounded-full text-sm px-3.5 py-1 text-center"
                 >
-                  <img
-                    alt={item.name}
-                    src={item.url}
-                    className="w-full rounded-lg"
-                  />
-                  <div className="absolute top-0 left-0 w-full h-full flex flex-col p-5 bg-black/0 rounded-lg opacity-0 hover:opacity-100 hover:bg-black/50">
-                    <button
-                      type="button"
-                      onClick={() => onDeleteImage(item.id)}
-                      className="w-min ml-auto text-rose-500 border-rose-500 border font-medium rounded-full text-sm px-3.5 py-1 text-center"
-                    >
-                      delete
-                    </button>
-                    <h3 className="text-xl font-semibold text-white mt-auto">
-                      {item.name}
-                    </h3>
-                  </div>
-                </div>
-              ))}
-            </Fragment>
+                  delete
+                </button>
+                <h3 className="text-xl font-semibold text-white mt-auto">
+                  {item.name}
+                </h3>
+              </div>
+            </div>
           ))}
         </div>
       ) : (
